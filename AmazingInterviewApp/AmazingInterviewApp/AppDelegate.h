d0912dd0684865605721cbb366186ac388a132b0
Patrick Farrell <Patrick@unknown-b8:8d:12:35:c3:14.home>
2013-12-12 14:40:20 +0000

    created project in master branch

---
+++ AmazingInterviewApp/AmazingInterviewApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AmazingInterviewApp
+//
+//  Created by Patrick Farrell on 12/12/2013.
+//  Copyright (c) 2013 PatrickFarrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AmazingInterviewApp/AmazingInterviewAppTests/AmazingInterviewAppTests.m
@@ -0,0 +1,34 @@
+//
+//  AmazingInterviewAppTests.m
+//  AmazingInterviewAppTests
+//
+//  Created by Patrick Farrell on 12/12/2013.
+//  Copyright (c) 2013 PatrickFarrell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AmazingInterviewAppTests : XCTestCase
+
+@end
+
+@implementation AmazingInterviewAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
